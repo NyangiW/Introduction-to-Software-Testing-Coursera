c747cef4b3fed460fe0dbd1cf26772ec4e0656b3
NyangiW <anyangowinnie9@gmail.com>
2025-10-22 17:26:19 +0300

    Update DemoTest.java

--- Week 1/Building Unit Tests/DemoTest.java
+++ Week 1/Building Unit Tests/DemoTest.java
@@ -1,79 +1,192 @@
+/** * */package microwave;import static org.junit.Assert.*;
+
+import static org.junit.Assume.*;import java.util.ArrayList;
+
+import java.util.Arrays;import static org.hamcrest.Matchers.*;
+
+import org.junit.Before;import org.junit.runner.RunWith;
+
+import com.pholser.junit.quickcheck.From;
+
+import com.pholser.junit.quickcheck.Property;
+
+import com.pholser.junit.quickcheck.generator.InRange;
+
+import com.pholser.junit.quickcheck.generator.Size;
+
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+
+/** * @author whalen * */
+
+@RunWith(JUnitQuickcheck.class)
+
+public class TestMicrowave {    public Microwave microwave;  
+
+  @Before    public void setUp() {        ArrayList<Preset> presets = new ArrayList<Preset>();   
+
+     presets.add(new Preset("Popcorn", 60, 10));     
+
+   presets.add(new Preset("Pizza 1 slice", 55, 6));    
+
+    presets.add(new Preset("Pizza 2 slice", 100, 6));       
+
+ presets.add(new Preset("Chicken breast", 40, 7));       
+
+ presets.add(new Preset("Defrost 1 lb hamburger", 180, 5));     
+
+   microwave = new Microwave(new ModeController(), new DisplayController(1), presets);   
+
+ }   
+
+ public String digitsString(byte [] digits) {  
+
+      return "" +                digits[DisplayController.TENS_OF_MINUTES] +   
+
+             digits[DisplayController.MINUTES] +         
+
+       digits[DisplayController.TENS_OF_SECONDS] +      
+
+          digits[DisplayController.SECONDS];    }  
+
+  /** seconds_elapse     *     * This method allows 'time' seconds to elapse with the microwave.     *  
+
+   * @param time     * @throws Throwable     */  
 
 
+  public void secondsElapse(int time)  {    
 
-/**
- * The class containing your tests for the {@link Demo} class. Make sure you
- * test all methods in this class (including both {@link Demo#main(String[])}
- * and {@link Demo#isTriangle(double, double, double)}).
- */
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import static org.junit.Assert.*;
-import org.junit.Test;
+    // System.out.println("" + time + " seconds elapse.");   
 
-public class DemoTest {
-    @Test
-    public void test_is_triangle_1() {
-        assertTrue(Demo.isTriangle(3, 4, 5));
-    }
-    @Test
-    public void test_is_triangle_2() {
-        assertTrue(Demo.isTriangle(5, 12, 13));
-    }
-    @Test
-    public void test_is_triangle_3() {
-        assertTrue(Demo.isTriangle(5, 13, 12));
-    }
-    @Test
-    public void test_is_triangle_4() {
-        assertTrue(Demo.isTriangle(12, 5, 13));
-    }
-    @Test
-    public void test_is_triangle_5() {
-        assertTrue(Demo.isTriangle(12, 13, 5));
-    }
-    @Test
-    public void test_is_NOT_triangle_1() {
-        assertFalse(Demo.isTriangle(5, 7, 13));
-    }
-    @Test
-    public void test_is_NOT_triangle_2() {
-        assertFalse(Demo.isTriangle(5, 13, 7));
-    }
-    @Test
-    public void test_is_NOT_triangle_3() {
-        assertFalse(Demo.isTriangle(13, 5, 7));
-    }
-    @Test
-    public void test_is_NOT_triangle_4() {
-        assertFalse(Demo.isTriangle(13,7, 5));
-    }
-    @Test
-    public void test_is_NOT_triangle_5() {
-        assertFalse(Demo.isTriangle(13, 7, 5));
-    }
-    @Test
-    public void test_is_NOT_triangle_6() {
-        assertFalse(Demo.isTriangle(5, 9, 3));
-    }
-    @Test
-    public void test_is_NOT_triangle_7() {
-        assertFalse(Demo.isTriangle(1, 2, -1));
-    }
+     for (int i = 0; i < time*microwave.getTickRateInHz(); i++) {    
 
-    /*
-    @Test
-    public void test_main_program_1() {
-        ByteArrayInputStream in = new ByteArrayInputStream("5\n12\n13\n".getBytes());
-        System.setIn(in);
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
-        String consoleOutput = "Enter side 1:\n";
-        consoleOutput += "Enter side 2:\n";
-        consoleOutput += "Enter side 3:\n";
-        consoleOutput += "This is a triangle:\n";
-        assertEquals(consoleOutput, out.toString());
-    }
-*/
-}
+        microwave.tick();        }    }  
+
+
+  public int calcTime(byte [] digits) {    
+
+    return digits[DisplayController.TENS_OF_MINUTES] * 600 +            
+
+    digits[DisplayController.MINUTES] * 60 +             
+
+   digits[DisplayController.TENS_OF_SECONDS] * 10 +         
+
+       digits[DisplayController.SECONDS];    }  
+
+
+  public boolean timeIsNormal() {    
+
+    byte[] digits = microwave.digits();      
+
+  return (digits[DisplayController.TENS_OF_MINUTES] <= 5 &&         
+
+       digits[DisplayController.MINUTES] <= 9 &&               
+
+ digits[DisplayController.TENS_OF_SECONDS] <= 5 &&             
+
+   digits[DisplayController.SECONDS] <= 9);   
+
+ }   
+
+
+ public boolean timeIsOk() {  
+
+      byte[] digits = microwave.digits();   
+
+     for (byte digit: digits) {        
+
+    if (digit < 0 || digit > 9) {            
+
+
+    return false;            }   
+
+     }        return true;    } 
+
+   public void secondsElapseCheckNormal(int time)  {      
+
+  // System.out.println("" + time + " seconds elapse.");     
+
+   boolean prevInNormalTime = timeIsNormal();     
+
+   boolean currInNormalTime = false;     
+
+   for (int i = 0; i < time*microwave.getTickRateInHz(); i++) {      
+
+      microwave.tick();            assertTrue(timeIsOk()); 
+
+           currInNormalTime = timeIsNormal();    
+
+        if (prevInNormalTime) { assertTrue(currInNormalTime); }    
+
+        prevInNormalTime = currInNormalTime;    
+
+    }    }   
+
+    @Property(trials = 100)   
+
+ public void checkCookingTimes(@InRange(min="0",max="9") int digit1,                                  @InRange(min="0",max="9") int digit2,                                  @InRange(min="0", max="9") int digit3,                                  @InRange(min="0", max="9") int digit4,                                  @InRange(min = "0", max = "500") int time) throws Throwable {        microwave.setDoorOpen(false);        microwave.tick();        microwave.digitPressed(digit1);        microwave.tick();        microwave.digitPressed(digit2);        microwave.tick();        microwave.digitPressed(digit3);        microwave.tick();        microwave.digitPressed(digit4);        microwave.tick();        microwave.startPressed();        microwave.tick();        this.secondsElapse(time);        int totalTime = digit1 * 600 + digit2 * 60 + digit3 * 10 + digit4;        byte [] digits = microwave.digits();        int timeRemaining = calcTime(digits);        ModeController.Mode mode = microwave.getMode();        assertEquals(timeRemaining, Integer.max(totalTime-time, 0));
+
+        assertTrue((time < totalTime) ?  
+
+mode == ModeController.Mode.Cooking :             
+
+   mode == ModeController.Mode.Setup);    }  
+
+     @Property(trials = 100)  
+
+  public void checkPreset(@InRange(min="0",max="6") int preset) throws Throwable {    
+
+    try {            microwave.presetPressed(preset);            
+
+microwave.tick();            int arrayIndex = preset - 1;         
+
+   byte [] digits =microwave.digits();           
+
+ Preset p = microwave.getPresets().get(arrayIndex);         
+
+   assertTrue(calcTime(digits) == p.getTimeToCook());     
+
+       assertTrue(microwave.getPowerLevel() == p.getPowerLevel());        } 
+
+catch (IllegalArgumentException e) {       
+
+     assertTrue(preset < 1 || preset > microwave.presets.size());        }    }   
+
+ @Property(trials=100)    public void checkUnusualTimes(@InRange(min="0",max="9") int digit1,                                  @InRange(min="0",max="9") int digit2,                                  @InRange(min="0", max="9") int digit3,                                  @InRange(min="0", max="9") int digit4,                                  @InRange(min = "0", max = "500") int time) {        microwave.setDoorOpen(false);        microwave.tick();       
+
+ microwave.digitPressed(digit1);        
+
+microwave.tick();       
+
+ microwave.digitPressed(digit2);       
+
+ microwave.tick();      
+
+  microwave.digitPressed(digit3);     
+
+   microwave.tick();      
+
+  microwave.digitPressed(digit4);      
+
+  microwave.tick();       
+
+ microwave.startPressed();       
+
+ microwave.tick();       
+
+ this.secondsElapseCheckNormal(time);       
+
+int totalTime = digit1 * 600 + digit2 * 60 + digit3 * 10 + digit4;     
+
+   byte [] digits = microwave.digits();       
+
+ int timeRemaining = digits[0]*600 + digits[1]*60 + digits[2]*10 + digits[3];    
+
+    ModeController.Mode mode = microwave.getMode();     
+
+   assertEquals(timeRemaining, Integer.max(totalTime-time, 0));       
+
+ assertTrue((time < totalTime) ?  mode == ModeController.Mode.Cooking :         
+
+       mode == ModeController.Mode.Setup);  }}  
+
+Follow this post
